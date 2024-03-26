1cd8550ef99baf2fd857e9eeb9872860587d5d4f
Francisco Hernandez Mena <vb655@cummins.com>
2024-03-26 15:20:15 -0600

    new home page

--- slideshow_project/sliderapp/urls.py
+++ slideshow_project/sliderapp/urls.py
@@ -6,6 +6,7 @@ from sliderapp.views import demo, slider_image_handler
 app_name = 'sliderapp'
 
 urlpatterns = [
+      
     path('demo/', demo, name='demo'),
     path('slider-image-handler/', slider_image_handler, name='slider-image-handler'),
 ]
--- slideshow_project/slideshow/urls.py
+++ slideshow_project/slideshow/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('', views.slideshow, name='slideshow'),
     path('image/<int:image_id>/', views.image_detail, name='image_detail'),
     path('upload/', views.upload_presentation, name='upload_presentation'),
+   
 ]
